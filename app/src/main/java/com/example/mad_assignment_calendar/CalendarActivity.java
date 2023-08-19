aa46c339a940e864740c1297ff9d2ea93cb267d9
kolmidaddy <samanthaltw1221@1utar.my>
2023-08-19 18:41:53 +0800

    updated calendaract file

--- app/src/main/java/com/example/mad_assignment_calendar/CalendarActivity.java
+++ app/src/main/java/com/example/mad_assignment_calendar/CalendarActivity.java
@@ -5,12 +5,12 @@ import android.os.Bundle;
 import android.widget.CalendarView;
 import android.widget.TextView;
 
+import com.example.calendarmad.R;
+
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.TimeZone;
 import java.util.Locale;
-
-
 
 public class CalendarActivity extends AppCompatActivity {
 
@@ -27,16 +27,15 @@ public class CalendarActivity extends AppCompatActivity {
         CalendarView calendarView = findViewById(R.id.calendarView);
         calendarView.setDate(currentTime, true, true);
 
-
         // Format and display the current year, date, and time
         SimpleDateFormat dateFormat = new SimpleDateFormat("dd-MM-yyyy HH:mm:ss", Locale.getDefault());
-        dateFormat.setTimeZone(TimeZone.getTimeZone("UTF-8"));
+        dateFormat.setTimeZone(TimeZone.getTimeZone("GMT+8"));  // Corrected the timezone here
         String currentDateTime = dateFormat.format(calendar.getTime());
 
-        TextView dateTextView = findViewById(R.id.calendar_tv1); // Replace with your TextView's ID
+        TextView dateTextView = findViewById(R.id.calendar_tv1);
         dateTextView.setText(currentDateTime);
 
-//        TextView currentDateTextView = findViewById(R.id.currentDateTextView);
-//        dateTextView.setText(currentDateTime);
+        TextView currentDateTextView = findViewById(R.id.currentDateTextView);
+        currentDateTextView.setText("Current Date: " + currentDateTime.substring(0, 10)); // To display just the date
     }
 }
