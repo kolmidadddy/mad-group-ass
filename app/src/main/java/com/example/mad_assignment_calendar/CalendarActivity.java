b55588c9533020885b20a84cb9fe5657b28c1896
kolmidaddy <samanthaltw1221@1utar.my>
2023-08-22 13:45:47 +0800

    updated on 'delete' events for calendar

--- app/src/main/java/com/example/mad_assignment_calendar/CalendarActivity.java
+++ app/src/main/java/com/example/mad_assignment_calendar/CalendarActivity.java
@@ -3,6 +3,7 @@ package com.example.mad_assignment_calendar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.widget.CalendarView;
 import android.widget.TextView;
@@ -19,12 +20,14 @@ import android.widget.EditText;
 
 
 public class CalendarActivity extends AppCompatActivity {
+    private SharedPreferences sharedPreferences; // Declaration of sharedPreferences here
 
     @SuppressLint("SetTextI18n")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_calendar);
+        sharedPreferences = getSharedPreferences("events", MODE_PRIVATE);
 
         // Get current time in GMT+8 timezone
         Calendar calendar = Calendar.getInstance(TimeZone.getTimeZone("GMT+8"));
@@ -45,13 +48,27 @@ public class CalendarActivity extends AppCompatActivity {
         TextView currentDateTextView = findViewById(R.id.currentDateTextView);
         currentDateTextView.setText("Current Date: " + currentDateTime.substring(0, 10)); // To display just the date
 
-        calendarView.setOnDateChangeListener((view, year, month, dayOfMonth) -> showEventDialog(year, month, dayOfMonth));
+
+        calendarView.setOnDateChangeListener((view, year, month, dayOfMonth) -> {
+            String eventDate = dayOfMonth + "/" + (month+1) + "/" + year;
+            String eventDescription = sharedPreferences.getString(eventDate, null);
+            if (eventDescription != null) {
+                updateOrDeleteEvent(year, month, dayOfMonth, eventDescription);
+            } else {
+                showEventDialog(year, month, dayOfMonth);
+            }
+        });
     }
     private void saveEvent(int year, int month, int dayOfMonth, String eventDescription) {
         // Dummy implementation
         // TODO: Save the event to your storage solution (e.g. Database, SharedPreferences, etc.)
         String eventDate = dayOfMonth + "/" + (month+1) + "/" + year;  // month is 0-based
         System.out.println("Event on " + eventDate + ": " + eventDescription);
+    }
+
+    private void deleteEvent(int year, int month, int dayOfMonth) {
+        String eventDate = dayOfMonth + "/" + (month+1) + "/" + year;
+        sharedPreferences.edit().remove(eventDate).apply();
     }
 
     private void showEventDialog(int year, int month, int dayOfMonth) {
@@ -68,6 +85,26 @@ public class CalendarActivity extends AppCompatActivity {
         });
 
         builder.setNegativeButton("Cancel", null);
+        builder.show();
+    }
+
+    private void updateOrDeleteEvent(int year, int month, int dayOfMonth, String currentDescription) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Update/Delete Event");
+
+        final EditText input = new EditText(this);
+        input.setText(currentDescription);
+        builder.setView(input);
+
+        builder.setPositiveButton("Update", (dialog, which) -> {
+            String eventDescription = input.getText().toString();
+            saveEvent(year, month, dayOfMonth, eventDescription);
+        });
+
+        builder.setNegativeButton("Delete", (dialog, which) -> deleteEvent(year, month, dayOfMonth));
+
+        builder.setNeutralButton("Cancel", null);
+
         builder.show();
     }
 
